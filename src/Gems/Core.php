86c9fc20f801969bcc82ed746ca64cd8ad26ae71
KingBob <52259912+Kingbobs@users.noreply.github.com>
2023-06-25 03:41:56 -0600

    Update Core.php

--- src/Gems/Core.php
+++ src/Gems/Core.php
@@ -1,56 +1,95 @@
 <?php
-
 namespace Gems;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
 
-class Core extends PluginBase implements Listener {
+class Core extends PluginBase implements Listener
+{
+    private array $gems = [];
 
-	private static $gems = [];
+    public function onLoad(): void
+    {
+        $this->saveResource("config.yml");
+    }
 
-	public function onLoad() {
-		$this->getLogger()->info("§aLoading gems...");
-		$this->saveResource("config.yml", false);
-		$this->loadGems();
-	}
+    public function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->loadGems();
+    }
 
-	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("§eGems §7has been loaded successfully!");
-	}
+    public function onDisable(): void
+    {
+        // Plugin shutdown logic
+    }
 
-	public function loadGems() {
-		$configData = $this->getConfig()->getAll();
-		foreach ($configData as $shortName => $data) {
-			self::$gems[$shortName] = new Gem(
-				$shortName,
-				$data["name"],
-				$data["item"],
-				$data["tags"] ?? [],
-				$data["cooldown"],
-				$data["effects"]
-			);
-			$this->getLogger()->info("§7Loaded §e" . $shortName);
-		}
-	}
+    public function loadGems(): void
+    {
+        $configData = $this->getConfig()->getAll();
+        foreach ($configData as $shortName => $data) {
+            if (!isset($data["name"]) || !isset($data["item"]) || !isset($data["cooldown"]) || !isset($data["effects"])) {
+                $this->getLogger()->warning("Invalid gem data for: " . $shortName);
+                continue;
+            }
 
-	public function onHeld(PlayerItemHeldEvent $event) {
-		foreach ($event->getPlayer()->getInventory()->getContents() as $content) {
-			foreach (self::$gems as $name => $gem) {
-				$gem->checkName($event->getPlayer(), $content);
-			}
-		}
-	}
+            $gem = new Gem(
+                $shortName,
+                $data["name"],
+                $data["item"],
+                $data["cooldown"],
+                $data["effects"],
+                $data["tags"] ?? []
+            );
+            $this->gems[$shortName] = $gem;
+            $this->getLogger()->info("Loaded: " . $shortName);
+        }
+    }
 
-	public function onUse(PlayerInteractEvent $event) {
-		foreach (self::$gems as $name => $gem) {
-			$gem->onItemUse($event->getPlayer(), $event->getItem());
-			if ($gem->isGem($event->getItem())) {
-				$event->setCancelled();
-			}
-		}
-	}
+    /**
+     * @param PlayerItemHeldEvent $event
+     */
+    public function onHeld(PlayerItemHeldEvent $event): void
+    {
+        $player = $event->getPlayer();
+        $inventory = $player->getInventory();
+
+        if ($inventory === null) {
+            return;
+        }
+
+        foreach ($inventory->getContents() as $slot => $content) {
+            foreach ($this->gems as $name => $gem) {
+                $gem->checkName($player, $content, $slot);
+            }
+        }
+    }
+
+    /**
+     * @param PlayerInteractEvent $event
+     */
+    public function onUse(PlayerInteractEvent $event): void
+    {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+
+        foreach ($this->gems as $name => $gem) {
+            if ($gem->isGem($item)) {
+                $event->setCancelled();
+
+                if ($gem->checkCooldown($player)) {
+                    $gem->onItemUse($event);
+                } else {
+                    $cooldown = $gem->getRemainingCooldown($player);
+                    $player->sendMessage("§3Cooldown required, wait §b" . $cooldown . "§7 seconds");
+                }
+
+                break;
+            }
+        }
+    }
 }
+
