d6b243b73c1aa3f22c797a4b456616b87b5e42bc
KingBob <52259912+Kingbobs@users.noreply.github.com>
2023-06-24 20:14:51 -0600

    Update Core.php

--- src/Gems/Core.php
+++ src/Gems/Core.php
@@ -1,46 +1,56 @@
 <?php
+
 namespace Gems;
 
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemHeldEvent;
 
-class Core extends PluginBase implements Listener{
-	
+class Core extends PluginBase implements Listener {
+
 	private static $gems = [];
-	
-	public function onLoad(){
-		 $this->getLogger()->info("§aLoading gems...");
-	    $this->saveResource("config.yml", false);
-	    $this->loadGems();
+
+	public function onLoad() {
+		$this->getLogger()->info("§aLoading gems...");
+		$this->saveResource("config.yml", false);
+		$this->loadGems();
 	}
-	
-	public function onEnable(){
-	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-       $this->getLogger()->info("§eGems §7has been loaded successfully!");
+
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("§eGems §7has been loaded successfully!");
 	}
-	
-	public function loadGems(){
-	    foreach($this->getConfig()->getAll() as $shortName => $data){
-	       self::$gems[$shortName] = new Gem($shortName, $data["name"], $data["item"], isset($data["tags"]) ? $data["tags"] : [], $data["cooldown"], $data["effects"]);
-	       $this->getLogger()->info("§7Loaded §e".$shortName);
-	   }
+
+	public function loadGems() {
+		$configData = $this->getConfig()->getAll();
+		foreach ($configData as $shortName => $data) {
+			self::$gems[$shortName] = new Gem(
+				$shortName,
+				$data["name"],
+				$data["item"],
+				$data["tags"] ?? [],
+				$data["cooldown"],
+				$data["effects"]
+			);
+			$this->getLogger()->info("§7Loaded §e" . $shortName);
+		}
 	}
-	
-	public function onHeld(PlayerItemHeldEvent $event){
-        foreach($event->getPlayer()->getInventory()->getContents() as $content){
-	       foreach(self::$gems as $name => $gem){
-	         $gem->checkName($event->getPlayer(), $content);
-	      }
-      }
+
+	public function onHeld(PlayerItemHeldEvent $event) {
+		foreach ($event->getPlayer()->getInventory()->getContents() as $content) {
+			foreach (self::$gems as $name => $gem) {
+				$gem->checkName($event->getPlayer(), $content);
+			}
+		}
 	}
-	
-	public function onUse(PlayerInteractEvent $event){
-	    foreach(self::$gems as $name => $gem){
-	       $gem->onItemUse($event->getPlayer(), $event->getItem());
-	       if($gem->isGem($event->getItem())) $event->setCancelled();
-	    }
+
+	public function onUse(PlayerInteractEvent $event) {
+		foreach (self::$gems as $name => $gem) {
+			$gem->onItemUse($event->getPlayer(), $event->getItem());
+			if ($gem->isGem($event->getItem())) {
+				$event->setCancelled();
+			}
+		}
 	}
 }
